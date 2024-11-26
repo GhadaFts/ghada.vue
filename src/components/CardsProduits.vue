f598f4d0b1707ab194324d211f49d344f7de46b4
Bassem Kallel <kallelbassem@bizerte.r-iset.tn>
2024-11-26 18:23:33 +0100

    grid

--- src/components/CardsProduits.vue
+++ src/components/CardsProduits.vue
@@ -1,15 +1,68 @@
 <template>
-        <div class=" flex flex-col items-center justify-center bg-white shadow-md hover:scale-105 duration-300">
-            <a>
-                <img src="https://tonusol.com/img/cms/Sansevieria-pot.jpg" >
-            </a>
-            <div class=" px-4 py-3">
-                <span class=" text-gray-400 uppercase text-sm">Chrolofyle</span>
-                <p class=" text-lg font-bold block truncate capitalize">Ma7bes</p>
-                <del>
-                    <p class=" text-sm text-gray-600 ml-2">20Dt</p>
-                </del>
-            </div>
-        </div>
-
-</template>
+    <div class="flex flex-col items-center justify-center bg-white shadow-md hover:scale-105 duration-300" 
+      v-for="event in sliceTab" :key="event.id">
+      <a>
+        <img :src="event.poster" alt="Event Poster" />
+      </a>
+      <div class="px-4 py-3">
+        <span class="text-gray-400 uppercase text-sm">{{ event.title }}</span>
+        <p class="text-lg font-bold block truncate capitalize">Ma7bes</p>
+        <del>
+          <p class="text-sm text-gray-600 ml-2">20Dt</p>
+        </del>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      nombreProduit: {
+        type: Number,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        events: [
+          {
+            id: 123,
+            title: "Cat Adoption Day",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 456,
+            title: "Community Gardening",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+        ],
+      };
+    },
+    computed: {
+            sliceTab(){
+                return this.events.slice(0,this.nombreProduit);
+      },
+    },
+  };
+  </script>
+  
--- src/components/GridProduits.vue
+++ src/components/GridProduits.vue
@@ -1,27 +1,57 @@
 <template>
-    <div class=" mx-10 my-24">
-        <h1 class=" text-5xl text-center my-8 text-dark-green">Best Produits</h1>
-        <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
-                <CardsProduits></CardsProduits>
-                <CardsProduits></CardsProduits>
-                <CardsProduits></CardsProduits>
-        </div>
-
+    <div class="mx-10 my-24">
+      <h1 class="text-5xl text-center my-8 text-dark-green">Best Produits</h1>
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
+        <!-- Passe la prop nombreProduit au composant enfant CardsProduits -->
+        <CardsProduits :nombreProduit="3"/>
+      </div>
     </div>
-</template>
-<script>
-    
-
-    import CardsProduits from './CardsProduits.vue';
-    export default {
-        name: "Grid3Produits",
-        components: {
-            CardsProduits,
-        },
-        props: {
-            
-        }
-    };
-    
-    
-</script>
+  </template>
+  
+  <script>
+  import CardsProduits from './CardsProduits.vue';
+  
+  export default {
+    name: "Grid3Produits",
+    components: {
+      CardsProduits,
+    },
+    data() {
+      return {
+        events: [
+          {
+            id: 123,
+            title: "Cat Adoption Day",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 456,
+            title: "Community Gardening",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+          {
+            id: 789,
+            title: "Beach Cleanup",
+            poster: "https://tonusol.com/img/cms/Sansevieria-pot.jpg",
+          },
+        ],
+      };
+    },
+  };
+  </script>
+  
