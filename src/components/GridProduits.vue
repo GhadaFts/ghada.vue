e7031a50660a9a73509fce64f3bfb812747e64e6
Bassem Kallel <kallelbassem@bizerte.r-iset.tn>
2024-11-26 14:14:06 +0100

    title grid margin

--- src/components/GridProduits.vue
+++ src/components/GridProduits.vue
@@ -1,6 +1,6 @@
 <template>
     <div class=" mx-10 my-24">
-        <h1 class=" text-5xl text-center my-7 text-dark-green">Best Produits</h1>
+        <h1 class=" text-5xl text-center my-8 text-dark-green">Best Produits</h1>
         <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
                 <CardsProduits></CardsProduits>
                 <CardsProduits></CardsProduits>
