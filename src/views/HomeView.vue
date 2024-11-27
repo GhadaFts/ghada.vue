2473bc85b959b2a697c130e408d5e42cc3c29d5d
Youssef Limem <145502826+yousseflimem@users.noreply.github.com>
2024-11-27 19:38:08 +0100

    home view youssef

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
   -->
   
   <section
-    class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
+    class="relative bg-hero-pattern bg-cover bg-center bg-no-repeat bg-local sm:bg-fixed"
   >
     <div
       class="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
@@ -19,25 +19,24 @@
         <h1 class="text-3xl font-extrabold text-white sm:text-5xl">
           Let us find your
   
-          <strong class="block font-extrabold text-rose-500"> Forever Home. </strong>
+          <strong class="block font-extrabold green"> FAVORITE PLANT. </strong>
         </h1>
   
-        <p class="mt-4 max-w-lg text-white sm:text-xl/relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-          numquam ea!
+        <p class="mt-4 max-w-lg text-white text-sm text-left  font-bold">
+          CHLOROPHYL opens the doors to a trendy and modern plant world. Simple to set up and easy to adjust according to your desires, our resin pots bring a real plus to your interior or exterior decoration. You will find in this selection many flower pots available in different sizes and colors, as practical as they are decorative.
         </p>
   
         <div class="mt-8 flex flex-wrap gap-4 text-center">
           <a
             href="#"
-            class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+            class="block w-full rounded bg-green px-12 py-3 text-sm font-medium text-white shadow hover:bg-dark-green focus:outline-none focus:ring active:bg-dark-green sm:w-auto"
           >
             Get Started
           </a>
   
           <a
             href="#"
-            class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+            class="block w-full rounded bg-white px-12 py-3 text-sm font-medium green shadow hover:text-dark-green focus:outline-none focus:ring active:text-dark-green sm:w-auto"
           >
             Learn More
           </a>
@@ -46,6 +45,14 @@
     </div>
   </section>
 </template>
+<style scoped>
+.green {
+  color: #10b981;
+}
+.dark-green {
+  color: #047857;
+}
+</style>
 <script>
 
 </script>
