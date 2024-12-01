f0aeb43748a02b7ac5d1172395a1ab5c712eada8
Youssef Limem <145502826+yousseflimem@users.noreply.github.com>
2024-12-01 16:22:01 +0100

    cartview beta

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,7 +3,7 @@
       <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <div class="mx-auto max-w-3xl">
           <header class="text-center">
-            <h1 class="text-xl font-bold text-darkgreen sm:text-3xl">Your Cart</h1>
+            <h1 class="text-xl font-bold text-darkgreen sm:text-3xl">Your CART</h1>
           </header>
     
           <div class="mt-8">
